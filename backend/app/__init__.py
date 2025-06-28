b46a802a099dc6a20681319e081649fad93645b6
VericaG <georgievskaverica9@gmail.com>
2025-06-28 20:28:20 +0200

    Last Check

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,1 @@
-# $env:MONGO_HOST = "localhost"
-# $env:MONGO_PORT = "27017"
-# $env:MONGO_USER = "myuser"
-# $env:MONGO_PASS = "mypassword"
-# $env:MONGO_DB = "mydatabase"
-#
-# # sada startuj app
-# uvicorn main:app --reload
+
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,14 +1,3 @@
-# from pydantic import BaseModel
-#
-#
-# class SongSchema(BaseModel):
-#     name: str
-#     artist: str
-#     genre: str
-#     year_released: int
-#     album: str
-#     rating: float
-
 from pydantic import BaseModel
 
 class SongSchema(BaseModel):
